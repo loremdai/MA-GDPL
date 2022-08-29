9bd6cc772eae5c5340926ddf7a698efd8e08c917
loremdai <etienn3dai@gmail.com>
2022-08-29 18:45:18 +0800

    changes

--- learner.py
+++ learner.py
@@ -537,12 +537,13 @@ class Learner():
         s_usr = torch.from_numpy(np.stack(batch.state_usr)).to(device=DEVICE)
         a_usr = torch.from_numpy(np.stack(batch.action_usr)).to(device=DEVICE)
         s_usr_next = torch.from_numpy(np.stack(batch.state_usr_next)).to(device=DEVICE)
-        # r_usr = torch.Tensor(np.stack(batch.reward_usr)).to(device=DEVICE)
+        r_usr = torch.Tensor(np.stack(batch.reward_usr)).to(device=DEVICE)
         batchsz_usr = s_usr.size(0)
+        print("r_usr size {}".format(r_usr.size()))
 
         ternimal = torch.Tensor(np.stack(batch.mask)).to(device=DEVICE)
         r_glo = torch.Tensor(np.stack(batch.reward_global)).to(device=DEVICE)
-        print("r_glo size {}".format(r_glo.size()))
+
 
         # 2. update reward estimator
         inputs_sys = (s_sys, a_sys, s_sys_next)
@@ -559,8 +560,8 @@ class Learner():
         log_pi_old_sa_usr = self.policy_usr.get_log_prob(s_usr, a_usr).detach()
 
         r_sys = self.rewarder_sys.estimate(s_sys, a_sys, s_sys_next, log_pi_old_sa_sys).detach()
-        r_usr = self.rewarder_sys.estimate(s_sys, a_sys, s_sys_next, log_pi_old_sa_sys).detach()
-        print("r_usr size {}".format(r_usr.size()))
+        r_usr2 = self.rewarder_sys.estimate(s_sys, a_sys, s_sys_next, log_pi_old_sa_sys).detach()
+        print("r_usr2 size {}".format(r_usr2.size()))
 
         # 4. estimate V, A and V_td-target
         v_sys = self.vnet(s_sys, 'sys').squeeze(-1).detach()
